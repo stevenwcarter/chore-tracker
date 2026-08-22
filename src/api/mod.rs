2cb55b153233bcb9261b79b800777da9ea238988
Steven Carter <steve@javapl.us>
2026-08-22 15:33:02 -0400

    tidy(dead-code): delete never-mounted api_routes and its test handler [T44][T45]

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,23 +1,12 @@
 use axum::Json;
-use axum::http::HeaderMap;
+use axum::http::StatusCode;
 use axum::response::IntoResponse;
 use axum::response::Response;
-use axum::{Router, http::StatusCode, routing::get};
 use serde::Serialize;
-
-use crate::context::GraphQLContext;
 
 pub mod auth;
 pub mod graphql;
 pub mod images;
-
-pub fn api_routes(_context: GraphQLContext) -> Router {
-    Router::new().route("/test", get(test))
-}
-
-pub async fn test(_headers: HeaderMap) -> Result<impl IntoResponse, AppError> {
-    Ok("test")
-}
 
 pub fn err_wrapper<T: Serialize>(result: anyhow::Result<T>) -> impl IntoResponse {
     match result {
